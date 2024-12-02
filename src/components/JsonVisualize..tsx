a9f138bab38fc7ab4bd1ca196e3b6112aa24c04e
siddharth200119 <siddramnani@gmail.com>
2024-12-02 21:28:09 +0530

    added visualizer

--- src/components/JsonVisualize..tsx
+++ src/components/JsonVisualize..tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ArcherContainer, ArcherElement } from 'react-archer';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 type JsonVisualizeProps = {
     json: string;
@@ -18,9 +19,9 @@ const JsonVisualize: React.FC<JsonVisualizeProps> = ({ json }) => {
     function processData() {
         try {
             const parsed = JSON.parse(json);
-            let nodes:Node[] = [];
+            let nodes: Node[] = [];
             processNodes(parsed, nodes, null, 0);
-            let grouped_nodes:Record<number, Node[]> = groupNodesByLevel(nodes);
+            let grouped_nodes: Record<number, Node[]> = groupNodesByLevel(nodes);
             processNodes(parsed, nodes, null, 0);
             setGroupedNodes(grouped_nodes);
 
@@ -37,8 +38,9 @@ const JsonVisualize: React.FC<JsonVisualizeProps> = ({ json }) => {
         }
         nodes.push(node);
         if (Array.isArray(json)) {
-            for(const index in json){
-                processNodes(json[index], nodes, node, level+1)
+            for (const index in json) {
+                node.content = <div>mid</div>
+                processNodes(json[index], nodes, node, level + 1)
             }
         } else if (typeof json === "object") {
             let content = "";
@@ -51,15 +53,19 @@ const JsonVisualize: React.FC<JsonVisualizeProps> = ({ json }) => {
                         nodes.push(mid_node)
                         processNodes(json[key], nodes, mid_node, level + 1);
                     } else {
-                        node.content += `${key} \n`
-                        processNodes(json[key], nodes, node, level + 1);
+                        const nodeId = `${Date.now()}-${Math.random()}`;
+                        const sub_node: Node = { id: nodeId, targets: [], content: <div>{key}</div>, level: level + 1, mid_level: false }
+                        node.targets.push(sub_node.id);
+                        node.content = `${"Root"} \n`;
+                        nodes.push(sub_node)
+                        processNodes(json[key], nodes, sub_node, level + 2);
                     }
                 } else {
                     content += `${key}: ${json[key]} \n`;
                     node.content = <div>{content}</div>
                 }
             }
-        }else{
+        } else {
             node.content = <div>{json}</div>
         }
     }
@@ -71,48 +77,51 @@ const JsonVisualize: React.FC<JsonVisualizeProps> = ({ json }) => {
     const groupNodesByLevel = (nodes: Node[]) => {
         return nodes.reduce((groups: Record<number, Node[]>, node) => {
             groups[node.level] = groups[node.level] || [];
-            if(node.content !== undefined && node.content !== null && typeof node.content === "object"){
-                console.log(typeof node.content)
+            if (node.content !== undefined && node.content !== null && typeof node.content === "object") {
                 groups[node.level].push(node);
             }
             return groups;
         }, {});
     };
     return (
-        <div className="flex items-start justify-start w-full h-full">
-            <ArcherContainer strokeColor="red">
-                {Object.keys(groupedNodes).map((level) => (
-                    <>
-                        <div
-                            key={level}
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            {groupedNodes[Number(level)].map((node) => (
-                                <ArcherElement
-                                    key={node.id}
-                                    id={node.id}
-                                    relations={node.targets.map((target) => ({
-                                        targetId: target,
-                                        targetAnchor: 'top',
-                                        sourceAnchor: 'bottom',
-                                        style: {},
-                                    }))}
-                                >
-                                    <div style={{ margin: '0 10px', padding: '10px', border: '1px solid black' }}>
-                                        {node.content}
-                                        {node.mid_level? "mid":""}
+        // <TransformWrapper initialScale={1}
+        // minScale={0.1}
+        // maxScale={7}>
+        //     <TransformComponent>
+                <div className="flex items-start justify-start w-full max-h-full">
+                    {/* <div className="max-h-[90%] max-w-[90%] min-h-[90vh] min-w-[40vw] relative">
+                        <div className="absolute top-0 left-0"> */}
+                        <ArcherContainer strokeColor="red">
+                            {Object.keys(groupedNodes).map((level) => (
+                                <>
+                                    <div key={level} className="flex justify-center">
+                                        {groupedNodes[Number(level)].map((node) => (
+                                            <ArcherElement
+                                                key={node.id}
+                                                id={node.id}
+                                                relations={node.targets.map((target) => ({
+                                                    targetId: target,
+                                                    targetAnchor: 'top',
+                                                    sourceAnchor: 'bottom',
+                                                    style: {},
+                                                }))}
+                                            >
+                                                <div style={{ margin: '0 10px', padding: '10px', border: '1px solid black' }}>
+                                                    {node.content}
+                                                    {node.mid_level ? "mid" : ""}
+                                                </div>
+                                            </ArcherElement>
+                                        ))}
                                     </div>
-                                </ArcherElement>
+                                    {groupedNodes[Number(level)].length !== 0 ? <div className="min-h-44"></div> : null}
+                                </>
                             ))}
+                        </ArcherContainer>
                         </div>
-                        {groupedNodes[Number(level)].length !== 0? <div className="min-h-14"></div> : null}
-                    </>
-                ))}
-            </ArcherContainer>
-        </div>
+        //             </div>
+        //         </div>
+        //     </TransformComponent>
+        // </TransformWrapper>
     );
 };
 
